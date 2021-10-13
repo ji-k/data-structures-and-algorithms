36bc093d4996c5b322cd4cf8d87ed4f6b013b7c7
Ji <jikyung@gmail.com>
2021-10-12 22:05:03 -0400

    commit

--- valid_subsequence.py
+++ valid_subsequence.py
@@ -2,6 +2,7 @@
 # EASY
 
 """
+PROBLEM
 [] Given two arrays of integers, array and subsequence, write a function that determines whether the second array is a subsequence of the first one.
 [] A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array, but that are in the same order as they appear in the array.
 
